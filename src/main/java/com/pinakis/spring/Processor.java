03dea8eb505ebfdf848a5e1c0cd7e7385f45e6ba
PinakiKundu <pinakispecial@gmail.com>
2025-05-25 20:57:35 +0530

    update jdk11

--- src/main/java/com/pinakis/spring/Processor.java
+++ src/main/java/com/pinakis/spring/Processor.java
@@ -1,6 +1,5 @@
 package com.pinakis.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -9,15 +8,13 @@ import java.util.List;
 
 @Component
 public class Processor {
-    @Autowired
-    private PublishTime publishTime;
 
-    public List<PublishTime> setUpPublishedTime(){
+    public List<PublishTime> setUpPublishedTime() {
         List<PublishTime> publishTimes = new ArrayList<>();
-        publishTime.setPublishedOn(Instant.parse("2017-02-03T11:25:30.00Z"));
-        publishTimes.add(publishTime);
-        publishTime.setPublishedOn(Instant.parse("2018-07-28T20:38:00.00Z"));
-        publishTimes.add(publishTime);
+
+        publishTimes.add(new PublishTime().setPublishedOn(Instant.parse("2017-02-03T11:25:30.00Z")));
+        publishTimes.add(new PublishTime().setPublishedOn(Instant.parse("2018-07-28T20:38:00.00Z")));
+
         return publishTimes;
     }
 }
